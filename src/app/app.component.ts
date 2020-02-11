0a0e2ba0bbac197e9f69ccae6f849d722f9b483d
Tor Gilbertson <tor@wisetail.com>
2020-02-10 18:37:35 -0700

    set up fs picker open with default options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+
+import * as filestack from "filestack-js";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filestack-ng-webc';
+  alert: string = '';
+
+  openFilePicker() {
+    if(!environment.filestackApiKey) {
+      return this.alert = `
+        Set
+        FILESTACK_API_KEY
+        in .env prior to running npm start
+      `;
+    }
+    const filestackClient = filestack.init(environment.filestackApiKey);
+    return filestackClient.picker().open();
+  }
 }
