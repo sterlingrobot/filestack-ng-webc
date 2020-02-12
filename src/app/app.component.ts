31e2a6181ed46a8c1024f06c536a2f413aba4813
Tor Gilbertson <tor@wisetail.com>
2020-02-11 21:01:08 -0700

    add workaround for edge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,24 @@ import { environment } from '../environments/environment';
 
 import * as filestack from "filestack-js";
 
+/**
+ * MS Edge gets stuck in a loop when opening the local file picker,
+ *  due to the __shady_dispatchEvent that re-triggers the file input
+ *  button within the Filestack widget.  In this case, we attach
+ *  a handler onto that shady event and stopPropagation on it.
+ */
+const isEdgePolyfill: () => boolean = () => /Edge\/\d{2}/.test(window.navigator.userAgent) && 'ShadyDOM' in window;
+
+const attachEdgeHijack = (tries: number) => {
+  const input = document.getElementById('fsp-fileUpload');
+  const hasShadyEvents = input && '__shady_addEventListener' in input;
+  if (input && hasShadyEvents) {
+    return (input as any).__shady_addEventListener('click', e => e.stopPropagation());
+  }
+  // If the widget has not loaded yet, we set a timeout for recheck
+  return tries > 0 && setTimeout(attachEdgeHijack.bind(null, --tries), 200);
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -23,6 +41,9 @@ export class AppComponent {
     }
     const filestackClient = filestack.init(environment.filestackApiKey);
     return filestackClient.picker({
+
+      onOpen: isEdgePolyfill() ? attachEdgeHijack.bind(null, 10) : () => {},
+
       onUploadDone: response => {
         const { filesUploaded } = response;
         if(filesUploaded.length) {
